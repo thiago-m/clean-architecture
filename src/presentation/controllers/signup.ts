79a006106104a8747375e1b477c9d59f7cded701
Thiago Marciel F Silva <th1ago_m@hotmail.com>
2023-01-15 20:59:21 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,13 @@
 export class SignUpController {
   handle (httpRequest: any): any {
+    let notExists: string
+
+    if (!httpRequest.body.name) notExists = 'name'
+    if (!httpRequest.body.email) notExists = 'email'
+
     return {
       statusCode: 400,
-      body: new Error('Missing param: name')
+      body: new Error(`Missing param: ${notExists}`)
     }
   }
 }
